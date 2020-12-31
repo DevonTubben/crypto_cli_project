5f361a91f228695b63e254a0b0a54dfb51aee8f8
Devon Tubben <tubbend1@msu.edu>
2020-12-31 13:22:38 -0500

    added to cli file

--- lib/crypto_cli_project/cli.rb
+++ lib/crypto_cli_project/cli.rb
@@ -16,18 +16,36 @@ class CLI
     def greet(name)
         puts "Hello #{name}! Would you like to see some information about the different Cryptocurrencies?"
         puts "Input 'y' to see the list, 'exit' to leave."
+        menu 
+    end 
+ #print out list of all crypto
+    def crypto_list
+        #hard coded below. Will be switched using api.
+        ["Bitcoin", "Ethereum", "Litecoin"].each.with_index(1) do |cryptos, i|
+        puts "#{i}. #{cryptos}"
     end 
 
+    def goodbye 
+        puts "Thanks for checking out the different cryptos! Bye!"
+    end 
+
+    def invalid 
+        "Invalid output. Please try again"
+        menu 
+    end 
     #based on our user selection, either show a list of crypto, give them an error message if it doesnt understand, or exit the program.
     def menu
         selection = user_input 
 
         if selection == 'y' 
             # print crypto list 
+            crypto_list
         elsif selection == 'exit'
             # give the user a goodbye message
+            goodbye 
         else 
             #invalid output and make the user select again
-            puts "invalid"
+            invalid
     end 
+end 
 end 
